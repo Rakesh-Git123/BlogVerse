51b2710caac74b0b896346a542de9e37e892a7dd
Rakesh-Git123 <rakeshgit1234@gmail.com>
2025-06-06 14:16:08 +0530

    small changes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -70,15 +70,21 @@ export const login=async(req,res)=>{
       }
 }
 
-export const logout=async(req,res)=>{
+export const logout = async (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
-    res.status(200).json({ success:true,message: "Logged out successfully" });
+    res.cookie("jwt", "", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      expires: new Date(0),
+    });
+    res.status(200).json({ success: true, message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
-    res.status(500).json({ success:false,message: "Internal Server Error" });
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-}
+};
+
 
 export const updateProfile = async (req, res) => {
   try {
